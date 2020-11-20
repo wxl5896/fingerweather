81226c700868b9891becb7418fbf8b1643b06ef6
RJX <2118807730@qq.com>
2020-11-20 09:35:24 +0800

    上传成功

---
+++ app/src/main/java/com/example/fingerweather/ChooseAreaFragment.java
@@ -0,0 +1,71 @@
+package com.example.fingerweather;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fingerweather.db.City;
+import com.example.fingerweather.db.County;
+import com.example.fingerweather.db.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE=0;
+    public static final int LEVEL_CITY=1;
+    public static final int LEVEL_COUNTY=2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList=new ArrayList<>();
+    private List<Province> provinceList;
+    private List<City> cityList;
+    private List<County> countyList;
+    private Province selectedProvince;
+    private City selectedCity;
+    private int currentLevel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.choose_area,container,false);
+        titleText=(TextView) view.findViewById(R.id.title_text);
+        backButton=(Button) view.findViewById(R.id.back_button);
+        listView=(ListView) view.findViewById(R.id.list_view);
+        adapter=new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(currentLevel==LEVEL_PROVINCE){
+                    selectedProvince=provinceList.get(position);
+                    queryCities();
+                }
+                
+            }
+
+            private void queryCities() {
+            }
+        });
+    }
+}
